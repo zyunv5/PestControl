68477090f6744516a35bf5465b041d254e5f6f51
zyunv5 <515224526@qq.com>
2020-11-17 00:08:16 +0800

    init

---
+++ App.vue
@@ -0,0 +1,20 @@
+<!-- <template>
+	<image v-if="showLoading" class="launch-img" src="https://statich.yidianzixun.com/public/file/1605540741621/loading.png" alt="" mode="scaleToFill" />
+</template> -->
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
